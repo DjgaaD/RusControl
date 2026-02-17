e0698d8ee40632086f53feb60d4ddd7011f0456b
DjgaaD <DjgaaD@ya.ru>
2026-02-18 00:59:52 +0300

    Fix repo URL

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir -p /www/cgi-bin
 touch /etc/wifi_whitelist
 
 # Скачиваем файлы
-REPO="https://raw.githubusercontent.com/Gertner-VIP/wifi-manager/main"
+REPO="https://raw.githubusercontent.com/DjgaaD/RusControl/main"
 
 for f in block unblock devices schedule schedule_del whitelist wifi_block wifi_unblock wifi_block_all wifi_unblock_all block_all_now unblock_all_now; do
     wget -O "/www/cgi-bin/$f" "${REPO}/cgi-bin/${f}"
